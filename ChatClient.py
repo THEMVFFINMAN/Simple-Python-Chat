9b182a2d46018c2575b1d12ee38df4b83292bba3
JJ <joshuajordanlowe@gmail.com>
2015-06-12 15:53:31 -0700

    Update ChatClient.py

--- ChatClient.py
+++ ChatClient.py
@@ -1,9 +1,15 @@
 import socket
 
+# Put the Server host here
 HOST = 'HOST IP ADDRESS GOES HERE'
-PORT = 'Pick a port, I like things that end in 11, 50011'
+# Put the server port here
+PORT = 50011
+
+# Create the socket and connect it to the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
+# Wait for input, print out what the server sends, etc
 while 1:
     s.sendall(raw_input('>> '))
     data = s.recv(1024)
